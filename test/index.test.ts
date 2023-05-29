ec34856ece2281b20a32a9b5fe46f6c59c4ab205
Samuel Preston <samp.reston@outlook.com>
2023-05-29 21:43:23 +0100

    remove testing

--- test/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { AnalyticsData } from "../src";
-import { expect, test } from "vitest";
-
-test("Analytics Data is initialized", () => {
-  const data = new AnalyticsData();
-  expect(data).toBeDefined();
-});
