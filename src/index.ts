91fec9d5b0644aec75f332429494d8f1eb60fcd7
Samuel Preston <samp.reston@outlook.com>
2023-05-29 20:10:04 +0100

    add vitest

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 import sourceMapSupport from "source-map-support";
 sourceMapSupport.install();
+
+import { AnalyticsData } from "lib/analytics-data";
+
+export { AnalyticsData };
--- src/lib/analytics-data/index.ts
+++ src/lib/analytics-data/index.ts
@@ -1,1 +1,19 @@
-class AnalyticsData
+export class AnalyticsData {
+  public options = {};
+
+  constructor(options) {
+    this.options = options;
+  }
+
+  public async runReport(propertyId: string, options: any): Promise<any> {
+    const [url, body] = generateReportQuery(propertyId, options);
+
+    const runReportResponse = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(body),
+    });
+
+    const runReportJson = await runReportResponse.json();
+    return runReportJson;
+  }
+}
